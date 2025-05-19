cef6213a193fdad9a7d34b0591b28b4ed5442c46
Marquineo <marquineo2001@gmail.com>
2025-05-19 21:07:14 +0200

    s

--- src/app/services/entrenador.service.ts
+++ src/app/services/entrenador.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class EntrenadorService {
-    //urlApi = 'https://api.gymbroanalytics.xyz/users';
+    //urlApi = 'https://api.gymbroanalytics.xyz/users'; //URL Producción
     urlApi = "http://127.0.0.1:8000/users";  // URL base de la API LOCAL
 
   constructor(private http: HttpClient) {}
